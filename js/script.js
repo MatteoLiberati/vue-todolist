18d20269938afd8af2bec4a929fae6a9c8b64f2b
MatteoLiberati <matteoliberati@hotmail.com>
2021-04-22 15:41:58 +0200

    stampa elementi in DOM

--- js/script.js
+++ js/script.js
@@ -1,7 +1,42 @@
 const app = new Vue({
     el:"#app",
     mounted(){},
-    data:{},
+    data:{
+        lista:[
+            {
+                impegno: 'Fare la spesa',
+                compleatato: false,
+            },
+            {
+                impegno: 'Cambiare password Google',
+                compleatato: false,
+            },
+            {
+                impegno: 'Ordinare piante giardino',
+                compleatato: true,
+            },
+            {
+                impegno: 'Sistemare profilo',
+                compleatato: false,
+            },
+            {
+                impegno: 'Organizzare festa Lorenzo',
+                compleatato: true,
+            },
+            {
+                impegno: 'Aggiustare cassa audio macchina',
+                compleatato: false,
+            },
+            {
+                impegno: 'Realizzare nuovo logo',
+                compleatato: false,
+            },
+            {
+                impegno: 'Lavare macchine',
+                compleatato: false,
+            },
+    ]
+    },
     methods:{},
 
     // END APP VUE
